805123b184bf2733950e75d6d60bf51530c1c502
littlebears <haunted2@gmx.com>
2015-07-08 14:20:33 +0200

    fixed the mess

--- templates/views-view-fields--latest-articles--block.tpl.php
+++ templates/views-view-fields--latest-articles--block.tpl.php
@@ -1,22 +1,7 @@
+<?php /* "Latest Articles" view custom template */ ?>
 
-<?php 
-/*
- * A template to style the view "Latest Articles"
- * */ 
- ?>
-
-<div class="article-slide-left">
-<div class="article-slide-picture">
-<?php print $fields['field_image']->content; ?>
-</div>
-</div>
-
-<div class="article-slide-right">
-<div class="article-slide-title">
-<?php print $fields['title']->content; ?>
-</div>
-
-<div class="article-slide-body">
-<?php print $fields['body']->content;?>
-</div>
+<div id="slide-container">
+<div id="slide-title"><?php print $fields['title']->content; ?></div>
+<div id="slide-image"><?php print $fields['field_image']->content; ?></div>
+<div id="slide-text"><?php print $fields['body']->content;?></div>
 </div>
